2445b4f4376cbc58c7162a50c513c30aa7ef7ef1
602-11\cre <32r>
2026-01-20 12:05:13 +0900

    CardViewとCardAnimation更新

--- Assets/Scripts/Animation/CardAnimation.cs
+++ Assets/Scripts/Animation/CardAnimation.cs
@@ -1,21 +1,44 @@
 using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using System;
+using System.Threading;
+using UnityEngine;
 
 public class CardAnimation : IDisposable
 {
+    private readonly Vector3 _initialScale;
+    private readonly Vector3 _initialPosition;
+    private readonly Quaternion _initialRotation;
+    private readonly Transform _transform;
+
+    private Tween _flipTween;
+    private Tween _expansionTween;
+    private bool _isExpansioning;
+    private bool _isFlipping;
+    private CancellationTokenSource _flipCts;
+
+    public CardAnimation(Transform t)
+    {
+        _transform = t;
+        _initialScale = _transform.localScale;
+        _initialPosition = _transform.localPosition;
+        _initialRotation = _transform.localRotation;
+    }
+
     public async UniTask FlipToFront()
     {
+        CancelFlip();
 
     }
 
     public async UniTask FlipToBack()
     {
-
+        CancelFlip();
     }
 
     public async UniTask FlipToHidden()
     {
-
+        CancelFlip();
     }
 
     public async UniTask OnSpawned()
@@ -23,9 +46,37 @@ public class CardAnimation : IDisposable
 
     }
 
-    public async UniTask OnTouched()
+    private void CancelFlip()
     {
+        _flipCts?.Cancel();
+        _flipCts?.Dispose();
+        _flipCts = null;
 
+        if(_flipTween!=null && _flipTween.IsActive())
+        {
+            _flipTween.Kill();
+            _flipTween = null;
+        }
+
+        _isFlipping = false;
+    }
+
+    public void Expansion()
+    {
+        if (_isExpansioning) return;
+        _isExpansioning = true;
+
+        ExpansionAsync().Forget();
+    }
+
+    private async UniTaskVoid ExpansionAsync()
+    {
+        _isExpansioning = false;
+    }
+
+    public async UniTaskVoid OnDestroyed()
+    {
+        
     }
 
     public void Dispose()
--- Assets/Scripts/Game/Views/CardView.cs
+++ Assets/Scripts/Game/Views/CardView.cs
@@ -1,5 +1,4 @@
 using Cysharp.Threading.Tasks;
-using Cysharp.Threading.Tasks.CompilerServices;
 using UnityEngine;
 
 public class CardView : MonoBehaviour
@@ -16,23 +15,31 @@ public class CardView : MonoBehaviour
 
     public int Value { get; private set; } = 0;
 
+    public void StopAllAnimations() => _cardAnimation?.Dispose();
+
+    public async UniTask PlayAnimationAndDestroy()
+    {
+        await _cardAnimation.OnDestroyed();
+        Destroy(gameObject);
+    }
+
     public async UniTask FlipToFront(int value)
     {
-        _state = State.Front;
         Value = value;
         await _cardAnimation.FlipToFront();
+        _state = State.Front;
     }
 
     public async UniTask FlipToBack()
     {
-        _state = State.Back;
         await _cardAnimation.FlipToBack();
+        _state = State.Back;
     }
 
     public async UniTask FlipToHidden()
     {
-        _state = State.Hidden;
         await _cardAnimation.FlipToHidden();
+        _state = State.Hidden;
     }
 
     private async UniTaskVoid InitializeAsync()
@@ -42,8 +49,14 @@ public class CardView : MonoBehaviour
 
     private void Start() => InitializeAsync().Forget();
 
+    private void Awake()
+    {
+        _cardAnimation = new CardAnimation(transform);
+    }
+
     private void OnMouseEnter()
     {
-        if(‚Í‚¢‚Ç‚¢‚ª‚¢)
+        if (_state != State.Back) return;
+        _cardAnimation.Expansion();
     }
 }
